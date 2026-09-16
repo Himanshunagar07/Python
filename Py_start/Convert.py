02635e1d649cfaac370c753690d59d7e9870d544
Himanshunagar07 <himanshunagar5507@gmail.com>
2026-09-16 11:26:48 +0530

    Add files via upload

---
+++ Py_start/Convert.py
@@ -0,0 +1,14 @@
+# Convert string to integer
+num_str = "10"
+num_int = int(num_str)
+print(num_int)
+
+# Convert integer to s
+num = 25
+num_str = str(num)
+print(num_str)
+
+# Convert float to integer 
+pi = 3.14
+pi_int = int(pi)
+print(pi_int)
---
+++ Py_start/main.py
@@ -0,0 +1,9 @@
+a = 34
+print(a)
+print(type(a))
+b = "14567"
+print(b)
+print(type(b))
+c = int(b)
+print(c)
+print(type(c))
