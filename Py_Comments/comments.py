06a689bd7b837b08d6bfe010e1113389014eea56
Himanshunagar07 <himanshunagar5507@gmail.com>
2026-09-16 11:27:47 +0530

    Add files via upload

---
+++ Py_Comments/comments.py
@@ -0,0 +1,24 @@
+# Comments
+# (#) This is a single-line comment in Python.
+'''
+# This is a multi-line comment in Python.
+'''
+
+# Escape Sequences
+#\n : New line
+#Example:
+print("Hello\nWorld")  # Output: Hello (new line) World
+#\t : Tab
+#Example:
+print("Hello\tWorld")  # Output: Hello (tab) World
+#\\ : Backslash
+#Example:
+print("This is a backslash: \\")  # Output: This is a backslash: \
+#\' : Single quote
+#Example:   
+print('It\'s a beautiful day!')  # Output: It's a beautiful day!
+#\" : Double quote
+#Example:
+print("She said, \"Hello!\"")  # Output: She said, "Hello!"
+
+print("Hello", "World", sep = ", ",end="!\n")
